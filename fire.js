b1b30c941a103a9d30e99c28ef477430793d2d3e
elias12vr <yoshytony0@gmail.com>
2025-07-04 18:44:13 -0600

    por seguridad borramos todo la configuracion de la api

--- fire.js
+++ fire.js
@@ -2,13 +2,13 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGBtsXfAmLwsNmhsLLTGMm_YYDGb3TSg4",
-  authDomain: "lockreminder-19626.firebaseapp.com",
-  databaseURL: "https://lockreminder-19626-default-rtdb.firebaseio.com",
-  projectId: "lockreminder-19626",
-  storageBucket: "lockreminder-19626.appspot.com",
-  messagingSenderId: "1077855779450",
-  appId: "1:1077855779450:web:d7a89e1dd33113a69259c3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
